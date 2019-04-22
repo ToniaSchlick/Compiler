373c3145bb0b01edc65a9d2241dff4389a7d734b
Jase Rost <jaserost@yahoo.com>
2019-04-22 17:42:44 -0600

    Finished part1 tiny code conversion

--- src/Driver.java
+++ src/Driver.java
@@ -24,11 +24,18 @@ public class Driver {
             //every element in this arraylist will be a single line of 3AC
             ArrayList<String> ac3 = ast.buildCode(tree.getGlobal());
             System.out.println(";IR CODE");
+
+            //print out the results of our 3AC generated code
             for (String line : ac3) {
                 System.out.println(";" + line);
             }
             System.out.println(";tiny code");
-            convertToTiny(ac3);
+            //convert 3AC into tiny code
+            ArrayList<String> tiny = convertToTiny(ac3);
+            //print the results of the tiny code conversion
+            for (String line : tiny) {
+                System.out.println(line);
+            }
         } catch (IOException e){
             System.out.println("Could not read file");
 
@@ -86,7 +93,59 @@ public class Driver {
         */
     }
 
-    private static void convertToTiny(ArrayList<String> ac3) {
-        
+    private static ArrayList<String> convertToTiny(ArrayList<String> ac3) {
+        ArrayList<String> tiny = new ArrayList<>();
+        for (String fullLine : ac3) {
+            String [] line = fullLine.split(" ");
+            if (line[0].contains("JUMP")) {
+
+            } else if (line[0].contains("LABEL")) {
+
+            } else if (line[0].equals("RET")) {
+                tiny.add("sys halt");
+            } else if (!line[0].equals("LINK")){
+                String datatype = line[0].substring(line[0].length()-1).toLowerCase();
+                //System.out.println(datatype);
+                if (datatype.equals("f")) {
+                    datatype = "r";
+                }
+                if (line[0].contains("STORE")) {
+                    tiny.add("move " + TtoR(line[1]) + " " + TtoR(line[2]));
+                } else if (line[0].contains("MULT")) {
+                    tiny.add("move " + TtoR(line[1]) + " " + TtoR(line[3]));
+                    tiny.add("mul" + datatype + " " + TtoR(line[2]) + " " + TtoR(line[3]));
+                } else if (line[0].contains("DIV")) {
+                    tiny.add("move " + TtoR(line[1]) + " " + TtoR(line[3]));
+                    tiny.add("div" + datatype + " " + TtoR(line[2]) + " " + TtoR(line[3]));
+                } else if (line[0].contains("ADD")) {
+                    tiny.add("move " + TtoR(line[1]) + " " + TtoR(line[3]));
+                    tiny.add("add" + datatype + " " + TtoR(line[2]) + " " + TtoR(line[3]));
+                } else if (line[0].contains("SUB")) {
+                    tiny.add("move " + TtoR(line[1]) + " " + TtoR(line[3]));
+                    tiny.add("sub" + datatype + " " + TtoR(line[2]) + " " + TtoR(line[3]));
+                } else if (line[0].contains("WRITE")) {
+                    tiny.add("sys write" + datatype + " " + line[1]);
+                } else if (line[0].contains("READ")) {
+                    tiny.add("sys read" + datatype + " " + line[1]);
+                } else if (line[0].contains("NE")) {
+
+                } else if (line[0].contains("EQ")) {
+
+                } else if (line[0].contains("GT")) {
+
+                } else if (line[0].contains("GE")) {
+
+                } else if (line[0].contains("LT")) {
+
+                } else if (line[0].contains("TE")) {
+
+                }
+            }
+        }
+        return tiny;
+    }
+
+    private static String TtoR(String t) {
+        return t.replace("$T", "r");
     }
 }
